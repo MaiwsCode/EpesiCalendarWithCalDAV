f34990b744f8911f48e3c65db18f704f7eee3beb
MaiwsCode <39537180+MaiwsCode@users.noreply.github.com>
2018-07-16 10:21:10 +0200

    fields

--- iCalSync/iCalSyncInstall.php
+++ iCalSync/iCalSyncInstall.php
@@ -6,17 +6,33 @@ class iCalSyncInstall extends ModuleInstall {
 	public function install() {
 // Here you can place installation process for the module
 		$ret = true;
+			$field = array('name' => _M('calendar url'), 'type'=>'text', 'required'=>false, 'visable'=>true,
+			'param'=>255);
+			Utils_RecordBrowserCommon::new_record_field('contact',$field);
+			$field = array('name' => _M('cal password'), 'type'=>'text', 'required'=>false, 'visable'=>true,
+			'param'=>255);
+			Utils_RecordBrowserCommon::new_record_field('contact',$field);
+			$field = array('name' => _M('uid'), 'type'=>'text', 'required'=>false, 'visable'=>false,
+			'param'=>255);
+			Utils_RecordBrowserCommon::new_record_field('phonecall',$field);
+			Utils_RecordBrowserCommon::new_record_field('crm_meeting',$field);
+			Utils_RecordBrowserCommon::new_record_field('task',$field);
 		Utils_RecordBrowserCommon::register_processing_callback('crm_meeting', array($this->get_type () . 'Common', 'on_action_meeting'));
 		Utils_RecordBrowserCommon::register_processing_callback('phonecall', array($this->get_type () . 'Common', 'on_action_phonecall'));
 		Utils_RecordBrowserCommon::register_processing_callback('task', array($this->get_type () . 'Common', 'on_action_task'));
             //    DB::Execute('CREATE TABLE public.users_calendar("ID" integer NOT NULL DEFAULT nextval("users_calendar_ID_seq"::regclass),user_id integer,adress_url text COLLATE pg_catalog."default",CONSTRAINT users_calendar_pkey PRIMARY KEY ("ID"))');
 		//Utils_RecordBrowserCommon::register_processing_callback('crm_calendar',array('iCalSyncCommon','add_action_bar'));
-		return $ret; // Return false on success and false on failure
+		return $ret; // Return true on success and false on failure
 	}
 
 public function uninstall() {
 // Here you can place uninstallation process for the module
 		$ret = true;
+		Utils_RecordBrowserCommon::delete_record_field('contact','calendar url');
+		Utils_RecordBrowserCommon::delete_record_field('contact','cal password');
+		Utils_RecordBrowserCommon::delete_record_field('crm_meeting','uid');
+		Utils_RecordBrowserCommon::delete_record_field('phonecall','uid');
+		Utils_RecordBrowserCommon::delete_record_field('task','uid');
              //   DB::Execute("DROP TABLE public.users_calendar");
 		return $ret; // Return false on success and false on failure
 	}
